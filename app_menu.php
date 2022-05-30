4d83a55e286dfe43b00626fc4a75313c20af7ddf
FusionPBX <markjcrane@gmail.com>
2022-05-30 17:00:36 -0600

    Create app_menu.php

---
+++ app_menu.php
@@ -0,0 +1,19 @@
+<?php
+
+$apps[$x]['menu'][0]['title']['en-us'] = 'Dialplan Tools';
+$apps[$x]['menu'][0]['title']['es-cl'] = '';
+$apps[$x]['menu'][0]['title']['fr-fr'] = '';
+$apps[$x]['menu'][0]['title']['fr-ca'] = '';
+$apps[$x]['menu'][0]['title']['pl'] = '';
+$apps[$x]['menu'][0]['title']['sv-se'] = '';
+$apps[$x]['menu'][0]['title']['uk'] = '';
+$apps[$x]['menu'][0]['title']['de-at'] = '';
+$apps[$x]['menu'][0]['uuid'] = '313c5304-e06f-4ac2-bdbb-4cec51ec294b';
+$apps[$x]['menu'][0]['parent_uuid'] = 'b94e8bd9-9eb5-e427-9c26-ff7a6c21552a';
+$apps[$x]['menu'][0]['category'] = 'internal';
+$apps[$x]['menu'][0]['path'] = '/app/dialplan_tools/dialplan_tools.php';
+$apps[$x]['menu'][0]['groups'][] = 'superadmin';
+//$apps[$x]['menu'][0]['groups'][] = 'admin';
+//$apps[$x]['menu'][0]['groups'][] = 'user';
+
+?>
