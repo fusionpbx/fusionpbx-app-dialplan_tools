4841fab90b79d540a77478fd224c0f3ef9392c37
Anthony <96078404+AntoniusDC@users.noreply.github.com>
2023-08-30 16:12:12 -0600

    Update Dialplan Tools app_menu.php

--- app_menu.php
+++ app_menu.php
@@ -1,13 +1,28 @@
 <?php
 
 $apps[$x]['menu'][0]['title']['en-us'] = 'Dialplan Tools';
-$apps[$x]['menu'][0]['title']['es-cl'] = 'Herramientas del plan de marcación';
-$apps[$x]['menu'][0]['title']['fr-fr'] = 'Outils de plan de numérotation';
-$apps[$x]['menu'][0]['title']['fr-ca'] = 'Outils de plan de numérotation';
-$apps[$x]['menu'][0]['title']['pl'] = 'Narzędzia planu wybierania';
-$apps[$x]['menu'][0]['title']['sv-se'] = 'Dialplan verktyg';
-$apps[$x]['menu'][0]['title']['uk'] = 'Інструменти Dialplan';
-$apps[$x]['menu'][0]['title']['de-at'] = 'Dialplan Tools';
+$apps[$x]['menu'][0]['title']['en-gb'] = 'Dialplan Tools';
+$apps[$x]['menu'][0]['title']['ar-eg'] = 'أدوات الطلب الهاتفي';
+$apps[$x]['menu'][0]['title']['de-at'] = 'Dialplan-Tools';
+$apps[$x]['menu'][0]['title']['de-ch'] = 'Dialplan-Tools';
+$apps[$x]['menu'][0]['title']['de-de'] = 'Dialplan-Tools';
+$apps[$x]['menu'][0]['title']['es-cl'] = 'herramientas de plan de marcado';
+$apps[$x]['menu'][0]['title']['es-mx'] = 'herramientas de plan de marcado';
+$apps[$x]['menu'][0]['title']['fr-ca'] = 'outils de plan de numérotation';
+$apps[$x]['menu'][0]['title']['fr-fr'] = 'outils de plan de numérotation';
+$apps[$x]['menu'][0]['title']['he-il'] = 'כלי תוכנית חיוג';
+$apps[$x]['menu'][0]['title']['it-it'] = 'strumenti del piano di numerazione';
+$apps[$x]['menu'][0]['title']['nl-nl'] = 'Telefoonplanhulpmiddelen';
+$apps[$x]['menu'][0]['title']['pl-pl'] = 'Narzędzia planu wybierania numerów';
+$apps[$x]['menu'][0]['title']['pt-br'] = 'Ferramentas do plano de discagem';
+$apps[$x]['menu'][0]['title']['pt-pt'] = 'Ferramentas do plano de discagem';
+$apps[$x]['menu'][0]['title']['ro-ro'] = 'Instrumente Dialplan';
+$apps[$x]['menu'][0]['title']['ru-ru'] = 'инструменты плана нумерации';
+$apps[$x]['menu'][0]['title']['uk-ua'] = 'інструменти плану номерів';
+$apps[$x]['menu'][0]['title']['sv-se'] = 'verktyg för uppringningsplan';
+$apps[$x]['menu'][0]['title']['zh-cn'] = '拨号计划工具';
+$apps[$x]['menu'][0]['title']['ja-jp'] = 'ダイヤル プラン ツール';
+$apps[$x]['menu'][0]['title']['ko-kr'] = '다이얼 플랜 도구';
 $apps[$x]['menu'][0]['uuid'] = '313c5304-e06f-4ac2-bdbb-4cec51ec294b';
 $apps[$x]['menu'][0]['parent_uuid'] = 'b94e8bd9-9eb5-e427-9c26-ff7a6c21552a';
 $apps[$x]['menu'][0]['category'] = 'internal';
