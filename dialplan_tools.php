b9c68e2ff54a91423b2e7b39f00e35d9bfae72e0
Alex <40072887+alexdcrane@users.noreply.github.com>
2024-09-09 10:01:09 -0700

    Update dialplan_tools.php

--- dialplan_tools.php
+++ dialplan_tools.php
@@ -235,6 +235,7 @@
 	echo "<input type='hidden' id='action' name='action' value=''>\n";
 	echo "<input type='hidden' name='search' value=\"".escape($search ?? null)."\">\n";
 
+	echo "<div class='card'>\n";
 	echo "<table class='list'>\n";
 	echo "<tr class='list-header'>\n";
 	if (permission_exists('dialplan_tool_add') || permission_exists('dialplan_tool_edit') || permission_exists('dialplan_tool_delete')) {
@@ -304,6 +305,7 @@
 	}
 
 	echo "</table>\n";
+	echo "</div>\n";
 	echo "<br />\n";
 	echo "<div align='center'>".$paging_controls."</div>\n";
 	echo "<input type='hidden' name='".$token['name']."' value='".$token['hash']."'>\n";
