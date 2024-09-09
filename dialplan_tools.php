3acf6fefacb61f9502a74a06ffb0b47c889c8ed2
Alex <40072887+alexdcrane@users.noreply.github.com>
2024-09-09 09:27:28 -0700

    Added the heading counter div

--- dialplan_tools.php
+++ dialplan_tools.php
@@ -184,7 +184,7 @@
 
 //show the content
 	echo "<div class='action_bar' id='action_bar'>\n";
-	echo "	<div class='heading'><b>".$text['title-dialplan_tools']." (".$num_rows.")</b></div>\n";
+	echo "	<div class='heading'><b>".$text['title-dialplan_tools']."</b><div class='count'>".number_format($num_rows)."</div></div>\n";
 	echo "	<div class='actions'>\n";
 	if (permission_exists('dialplan_tool_add')) {
 		echo button::create(['type'=>'button','label'=>$text['button-add'],'icon'=>$_SESSION['theme']['button_icon_add'],'id'=>'btn_add','name'=>'btn_add','link'=>'dialplan_tool_edit.php']);
